cf9a62ccc2b1772c6550e46a5f22aae2b44efd64
Somya1911 <khatrisomya18@gmail.com>
2021-03-20 23:19:42 +0530

    Practice

---
+++ QuestionOne.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+/**
+ * @author Somya Khatri
+ * @version 1.0
+ * @param a - first numeric value for the addNumbers operation
+ * @param b - second numeric value for the addNumbers operation
+ * @param c - first numeric value for the subtractNumbers operation
+ * @param d - second numeric value for the subtractNumbers operation
+ * @return sum of two numbers
+ * @return difference of two numbers
+*/
+
+public class Main {
+
+    
+    public int addNumbers(int a, int b) {
+      int sum = a + b;
+      return sum;
+    }
+
+    public int subtractNumbers(int c, int d) {
+        int diff = c - d;
+        return diff;
+    }
+  
+    public static void main(String[] args) {
+      
+      int num1 = 25;
+      int num2 = 15;
+  
+      
+      Main obj = new Main();
+      int result_1 = obj.addNumbers(num1, num2);
+      System.out.println("Sum is: " + result_1);
+      int result_2 = obj.subtractNumbers(num1, num2);
+      System.out.println("Diff is: " + result_2);
+    }
+  }
---
+++ arr.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.util.Arrays;
+public class arr
+{
+  int u=0;
+  public int[] xor(int[] a,int[] b)
+  {
+      int d=0;
+      int n=a.length+b.length;
+      int dup[]=new int[n];
+      int uni[]= new int[n];
+
+     
+    for(int i=0;i<a.length;i++) 
+    {
+        for(int j=0;j<b.length;j++)
+        {
+         if(a[i] == b[j])
+         {
+             dup[d]=a[i];
+             d++;
+         }
+        }
+    }
+ 
+    
+    for(int i=0;i<a.length;i++)
+    {
+        int e=1;
+        for(int j=0;j<d;j++)
+        {
+            if(a[i] == dup[j])
+                e=0;
+        }
+     if(e==1)
+     {
+         uni[u++]=a[i];
+     }
+    }
+
+ 
+    for(int i=0;i<b.length;i++)
+    {
+        int e=1;
+        for(int j=0;j<d;j++)
+        {
+            if(b[i] == dup[j])
+                e=0;
+        }
+     if(e==1)
+     {
+         uni[u]=b[i];
+         u++;
+     }
+    }
+
+    return uni;
+  }
+  
+	public static void main(String args[])
+	{
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of two arrays");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt(); 
+        int[] x=new int[n1];
+        int[] y=new int[n2];
+        System.out.println("Enter input in 1st array");
+        for(int i=0;i<n1;i++)
+        {
+           x[i]=sc.nextInt();
+        }
+         System.out.println("Enter input in 2nd array");
+        for(int i=0;i<n2;i++)
+        {
+           y[i]=sc.nextInt();
+        }
+
+        arr ob=new arr();
+        int[] result=ob.xor(x,y);
+        Arrays.sort(result,0,ob.u);
+        System.out.println("Elements are:");
+        for(int i=0;i<ob.u;i++)
+        {
+        System.out.print(result[i]+"\t");
+        }
+
+	}
+}
